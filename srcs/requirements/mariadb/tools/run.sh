ce1bb3bdd4ae8ad038710ee21f9debe30534f442
aerragha <erraghayayoub@gmail.com>
2022-04-02 13:08:14 +0000

    finish mariadb container

--- srcs/requirements/mariadb/tools/run.sh
+++ srcs/requirements/mariadb/tools/run.sh
@@ -1,28 +1,25 @@
-#!/bin/ash
-
-apk update && apk upgrade && apk add mariadb mariadb-client openrc
-
-if [ ! -d "/run/mysqld" ]; then
-	mkdir -p /run/mysqld
-	chown -R mysql:mysql /run/mysqld
-fi
-
-if [ ! -d "/var/lib/mysql/mysql" ]; then
-	
-	chown -R mysql:mysql /var/lib/mysql
-
-	# init database
-	mysql_install_db --basedir=/usr --datadir=/var/lib/mysql --user=mysql --rpm > /dev/null
-fi
-
-
-
-
-
-# apk update && apk upgrade && apk add mariadb mariadb-client openrc
+#!/bin/sh
 
 # mkdir -p /run/openrc/
 # touch /run/openrc/softlevel
 
-# /usr/bin/mysql_install_db --user=mysql
-# /usr/bin/mysqld_safe --datadir="/var/lib/mysql"
+if [ ! -f "/var/lib/mysql/ib_buffer_pool" ];
+then
+	/etc/init.d/mariadb setup
+	/etc/init.d/mariadb start
+
+	# mysql -u ${MYSQL_ROOT} < data.sql
+	# rm data.sql
+	mysql -u ${MYSQL_ROOT} -e "CREATE USER '${MYSQL_USER}'@'localhost' IDENTIFIED BY '${MYSQL_PASSWORD}';"
+	mysql -u ${MYSQL_ROOT} -e "GRANT ALL PRIVILEGES ON *.* TO '${MYSQL_USER}'@'localhost' IDENTIFIED BY '${MYSQL_PASSWORD}';"
+
+	mysql -u ${MYSQL_ROOT} -e "CREATE USER '${MYSQL_USER}'@'%' IDENTIFIED BY '${MYSQL_PASSWORD}';"
+	mysql -u ${MYSQL_ROOT} -e "GRANT ALL PRIVILEGES ON ${MYSQL_DATABASE}.* TO '${MYSQL_USER}'@'%' IDENTIFIED BY '${MYSQL_PASSWORD}';"
+
+	mysql -u ${MYSQL_ROOT} -e "ALTER USER '${MYSQL_USER}'@'localhost' IDENTIFIED BY '${MYSQL_PASSWORD}';"
+	mysql -u ${MYSQL_ROOT} -e "ALTER USER '${MYSQL_ROOT}'@'localhost' IDENTIFIED BY '${MYSQL_ROOT_PASSWORD}';"
+	sed -i 's/skip-networking/# skip-networking/g' /etc/my.cnf.d/mariadb-server.cnf
+fi
+rc-service mariadb start
+rc-service mariadb stop
+/usr/bin/mariadbd --basedir=/usr --datadir=/var/lib/mysql --plugin-dir=/usr/lib/mariadb/plugin --user=mysql --pid-file=/run/mysqld/mariadb.pid
