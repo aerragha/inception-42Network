d505b0ef9dd512785670776d04624a407a6cb686
aerragha <erraghayayoub@gmail.com>
2022-04-06 14:01:03 +0000

    remove ssl & finish ftp-server

---
+++ Makefile
@@ -0,0 +1,2 @@
+# docker-compose down && docker system prune -f && rm -rf /home/aerragha/data/*
+# docker-compose up -d --build
--- srcs/requirements/bonus/ftp-server/tools/script.sh
+++ srcs/requirements/bonus/ftp-server/tools/script.sh
@@ -1,10 +1,14 @@
 #!/bin/sh
-tail -f /dev/null
-# create new user
-# adduser -D ${FTP_SERVER_USER}
 
-# # Edit password for the created user 
-# echo ${FTP_SERVER_USER}:${FTP_SERVER_PASS} | chpasswd
+#create new user
+adduser -D ${FTP_SERVER_USER}
 
-# # Run & stay in the foreground
-# /usr/bin/vsftpd /etc/vsftpd/vsftpd.conf
+# Edit password for the created user 
+echo ${FTP_SERVER_USER}:${FTP_SERVER_PASS} | chpasswd
+
+chown -R ${FTP_SERVER_USER}:${FTP_SERVER_USER} /home/"${FTP_SERVER_USER}"
+
+echo "${FTP_SERVER_USER}" | tee -a /etc/vsftpd.userlist
+
+# Run & stay in the foreground
+/usr/sbin/vsftpd /etc/vsftpd/vsftpd.conf
