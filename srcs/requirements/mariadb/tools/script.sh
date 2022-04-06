62ec12af90e6bc3641c379585c3cba9e6ac0ac53
aerragha <erraghayayoub@gmail.com>
2022-04-06 15:21:46 +0000

    clean files

--- srcs/requirements/mariadb/tools/script.sh
+++ srcs/requirements/mariadb/tools/script.sh
@@ -10,15 +10,15 @@ then
 
 	mysql -u ${MYSQL_ROOT} < database.sql
 
-	# User with privileges to all databases
+	
 	mysql -u ${MYSQL_ROOT} -e "CREATE USER '${MYSQL_USER}'@'localhost' IDENTIFIED BY '${MYSQL_PASSWORD}';"
 	mysql -u ${MYSQL_ROOT} -e "GRANT ALL PRIVILEGES ON *.* TO '${MYSQL_USER}'@'localhost' IDENTIFIED BY '${MYSQL_PASSWORD}';"
 
-	# User with privileges to wordpress databases
+	
 	mysql -u ${MYSQL_ROOT} -e "CREATE USER '${MYSQL_USER}'@'%' IDENTIFIED BY '${MYSQL_PASSWORD}';"
 	mysql -u ${MYSQL_ROOT} -e "GRANT ALL PRIVILEGES ON ${MYSQL_DATABASE}.* TO '${MYSQL_USER}'@'%' IDENTIFIED BY '${MYSQL_PASSWORD}';"
 
-	# Flush privileges will reloads the grant tables in the mysql database enabling the changes to take effect without reloading or restarting mysql service.
+	
 	mysql -u ${MYSQL_ROOT} -e "FLUSH PRIVILEGES;"
 fi
 
